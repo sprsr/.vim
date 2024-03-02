6182f2081cd8794b4c6bed358ae15186522caf12
sprsr <127357996+sprsr@users.noreply.github.com>
2024-03-02 16:53:13 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -12,74 +12,135 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-"smoka7/hop.nvim",
-"alec-gibson/nvim-tetris",
-'kosayoda/nvim-lightbulb',
-"DNLHC/glance.nvim",
-"neovim/nvim-lspconfig",
+
+-- Mason Registry Manager
 {
-    'nvimdev/lspsaga.nvim',
-    config = function()
-        require('lspsaga').setup({})
-    end,
+"williamboman/mason.nvim",
+run = ":MasonUpdate" -- Update registry contents
+},
+{"neovim/nvim-lspconfig"},
+
+-- Alpha Menu
+{
+    "goolord/alpha-nvim",
+    requires = { "nvim-tree/nvim-web-devicons" },
+    config = function ()
+        require'alpha'.setup(require'alpha.themes.startify'.config)
+    end
+},
+
+-- nvim-cmp 
+-- Full support for LSP completion with minimal dependancies
+{"hrsh7th/cmp-nvim-lsp"},
+{"hrsh7th/cmp-buffer"},
+{"hrsh7th/cmp-path"},
+{"hrsh7th/cmp-cmdline"},
+{"hrsh7th/nvim-cmp"},
+
+-- nvim-web-devicons
+{"nvim-tree/nvim-web-devicons"},
+
+-- Tetris
+{"alec-gibson/nvim-tetris"},
+
+-- Cellular Automation
+{
+    "eandrju/cellular-automaton.nvim",
     dependencies = {
-        "neovim/nvim-lspconfig"
+        "nvim-treesitter/nvim-treesitter"
     }
-}
+},
+-- Blackjack
+-- {
+-- 'alanfortlink/blackjack.nvim',
+--  requires = {'nvim-lua/plenary.nvim'},
+-- }
 })
 
 -- Setup language servers.
 local lspconfig = require('lspconfig')
 
 -- Configure the C/C++/Objective-C language server
-lspconfig.sourcekit.setup{}
--- Configure the System Verilog language server
-lspconfig.svlangserver.setup{}
--- Configure the python language server
-lspconfig.pyright.setup{}
+ lspconfig.clangd.setup{
+ }
 
+ lspconfig.cpptools.setup{
+     settings= {
+     	cmd = "/home/scpresse/.local/share/nvim/mason/bin/OpenDebugAD7"
+     }
+ }
+-- setup mason
+require("mason").setup()
 
--- Global mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+-- setup cmp
+  -- Set up nvim-cmp.
+  local cmp = require'cmp'
 
--- Use LspAttach autocommand to only map the following keys
--- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+  cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
 
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
-  end,
-})
+  -- Set configuration for specific filetype.
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+    }, {
+      { name = 'buffer' },
+    })
+  })
 
-require("nvim-lightbulb").setup({
-  autocmd = { enabled = true }
-})
+  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
+
+  -- Set up lspconfig.
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
+  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+   require('lspconfig')['clangd'].setup {
+   capabilities = capabilities
+   }
 
 -- Source init.vim
 vim.cmd('source ~/.config/nvim/classic.vim')
